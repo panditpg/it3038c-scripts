e7bc424c526ca27437cad4dd435abeb571a5dc6e
Prayag Pandit <panditpg@mail.uc.edu>
2020-09-21 17:10:38 -0400

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,28 @@
+﻿function getIP {
+    (Get-NetIPAddress -InterfaceAlias Ethernet0).ipv4address
+}
+
+$IP = getIP
+
+$Body = "This machine's IP is {0}." -f $IP
+
+write-host($Body)
+
+
+$Body += "User is {0}." -f $env:USERNAME
+Write-Host($Body)
+
+$HOSTNAME = "panditpg-win"
+$Body += "Hostname is {0}." -f $HOSTNAME
+Write-Host($Body)
+
+$Body += "PowerShell Version is {0}." -f $HOST.Version.Major
+Write-Host($Body)
+
+
+$Date = Get-Date -UFormat "%A, %B %d, %Y"
+$Body += "Today's date is {0}." -f $Date
+write-host($Body)
+
+#Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "panditpg@mail.uc.edu" -Subject "IT3038C Windows Sysinfo" -Body $Body -SmtpServer "Smtp.office365.com" -port 587 -UseSsl -Credential (Get-Credential)
+Send-MailMessage -To "prayag13@gmail.com" -From "prayag13@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $Body -SmtpServer "Smtp.gmail.com" -port 587 -UseSsl -Credential (Get-Credential)
