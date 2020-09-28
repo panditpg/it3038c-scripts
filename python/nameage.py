5ce332e614c7ad18527cb0e43fcc7fe789b53c57
Prayag Pandit <panditpg@mail.uc.edu>
2020-09-28 17:47:37 -0400

    commit

--- python/nameage.py
+++ python/nameage.py
@@ -1,6 +1,11 @@
+import time
+
+##### In class lab practice that shows sleep time with bunch of personal craps
 print("What's your name?")
 myName = input()
 print("Hello " + myName + ". That is a good name. How old are you?")
-myAge = input()
-print(myAge + "? That's funny, I'm only a few seconds old.")
-print("I wish I was " + str(int(myAge) * 2) + " years old") 
+myAge = int(input())
+print("%s? That's funny, I'm only a few seconds old." % myAge) 
+print("I wish I was %s years old" % (myAge * 2)) 
+time.sleep(3)
+print("I'm tired. I'm done. Goodnight! Catch you later.") 
