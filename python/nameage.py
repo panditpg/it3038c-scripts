1b9f04e3a24949b872f6ca156dfb826edc0ae957
Prayag Pandit <panditpg@mail.uc.edu>
2020-09-28 16:36:37 -0400

    commit

---
+++ python/nameage.py
@@ -0,0 +1,6 @@
+print("What's your name?")
+myName = input()
+print("Hello " + myName + ". That is a good name. How old are you?")
+myAge = input()
+print(myAge + "? That's funny, I'm only a few seconds old.")
+print("I wish I was " + str(int(myAge) * 2) + " years old") 
