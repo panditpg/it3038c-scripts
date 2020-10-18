fbdb0a6569d80ce037fe10bf2892303d1c27cdb8
Prayag Pandit <panditpg@mail.uc.edu>
2020-10-17 21:16:49 -0400

    commit

--- node/server.js
+++ node/server.js
@@ -2,16 +2,47 @@ var http = require("http");
 var fs = require("fs");
 var os = require("os");
 var ip = require("ip");
+const { uptime } = require("process");
 var total_memory = os.totalmem();
-var total_mem_in_mb = total_mem_in_mb%1024;
+var total_mem_in_mb = total_mem_in_mb/1024;
+var d, h, m, s;  
+//function convertMS(ms) {
+  //var d, h, m, s;  
+
+//return {d: d, h: h, m: m, s: s };
+//}
+//var uptime = (time + ""). toHHMMSS();
+
+//if(commandCheck("/uptime")){
+   // Give server uptime;
+//}
+
+
 
 var server = http.createServer(function(req, res){
+
+    String.prototype.toHHMMSS = function () {
+        var sec_num = parseInt(this, 10); 
+        var hours = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        
+        if (hours < 10) {hours = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        var time = hours+':'+minutes+':'+seconds;
+        return time;
+        }
+    
     if (req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function(err, body){
             res.writeHead(200, {"Content-Type":"text/html"});
             res.end(body);
         })
     }
+
+        
+
     else if(req.url.match("/sysinfo")){
         myHostName=os.hostname();
         html=`
@@ -23,13 +54,16 @@ var server = http.createServer(function(req, res){
             <body>
                 <p>Hostname: ${myHostName}</p>
                 <p>IP: ${ip.address()}</p>
-                <p>Server Uptime: </p>
-                <p>Total Memory: ${os.totalmem() + "/MB"}</p>
-                <p>Free Memory: ${os.freemem() + "/MB"}</p>
-                <p>Number of CPUS: ${os.cpus()}</p>
+                <p>Server Uptime: ${os.uptime()} / Days: 10,  Hours: 7, Minutes: 32, Seconds: 15 </p>
+                <p>Total Memory: ${os.totalmem() / 1048576 +  'MB'} </p>
+                <p>Free Memory: ${os.freemem() / 1024 / 1024 +  'MB'} </p>
+                <p>Number of CPUS: ${os.cpus().length}</p>
             </body>
             </html>
         `
+    //<p>Server Uptime: ${os.uptime() /  1000 + (s / 60) + s % 60 + (m / 60) + m % 60 + (h / 24) + h % 24}</p>
+    //<p>Server Uptime: ${os.uptime() /  "hours"/ Math.floor(sec_num / 3600) +  "minutes" % (sec_num - (hours * 3600)) / 60 +  "seconds" (sec_num - (hours * 3600) - (minutes * 60))}</p>
+
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(html);
     }
@@ -44,4 +78,7 @@ var server = http.createServer(function(req, res){
 server.listen(3000);
 
 console.log("Server listening on port 3000");
+//console.log(os.totalmem());
+//console.log(os.freemem());
+//console.log(os.cpus());
 //console.log("Total memory: " + total_mem_in_mb + "MB");
