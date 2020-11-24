b0c11313665c197bac98e8216f0737e6f97a8b92
Prayag Pandit <panditpg@mail.uc.edu>
2020-11-24 18:21:36 -0500

    clean up

--- python/lab7.py
+++ python/lab7.py
@@ -3,16 +3,29 @@
 ##venv/webscraping/scripts/activate.ps1
 ##pip install pillow
 
-from PIL import Image, ImageFilter
-import string
-myImage = Image.open
-myImage.load
+##from PIL import Image, ImageFilter
+##import string
+##myImage = Image.open
+##myImage.load
 
 
-myImage.format
-myImage.size
-myImage.show()
+##myImage.format
+##myImage.size
+##myImage.show()
 
 
-blur = myImage.filter(ImageFilter(BLUR)).show()
-quit()
+##blur = myImage.filter(ImageFilter(BLUR)).show()
+##quit()
+
+from PIL import Image;
+ 
+#Open existing image
+OriImage = Image.open('images/lamb.jpg')
+OriImage.show()
+
+#Applying BoxBlur filter
+boxImage = OriImage.filter(ImageFilter.BoxBlur(5))
+boxImage.show()
+
+#Save Boxblur image
+boxImage.save('images/boxblur.jpg')
